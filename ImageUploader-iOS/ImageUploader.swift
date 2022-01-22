d029d5efe72ba663ba244a60e0d0301e616183aa
Yusuphjoluwasen <Yusuphjoluwaseun@gmail.com>
2022-01-22 23:25:35 +0100

    edited podspec

---
+++ ImageUploader-iOS/ImageUploader.swift
@@ -0,0 +1,14 @@
+//
+//  ImageUploader.swift
+//  ImageUploader-iOS
+//
+//  Created by Guru on 22/01/2022.
+//
+
+import Foundation
+public class ImageUploader{
+    
+    public static func getImageUploader(){
+        print("image upload")
+    }
+}
